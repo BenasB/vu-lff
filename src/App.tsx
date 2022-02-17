1ba6e528e4d208ad4c08cf28575cf738743b3a31
BenasB <29711974+BenasB@users.noreply.github.com>
2022-02-17 23:32:00 +0200

    LFF: Programming
    Updated FAQ questions <START=23:00>

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import GitHubUrlForm from './GitHubUrlForm';
 const App: React.FC = () => {
   return (
     <>
-      <Container>
+      <Container className='mt-5' style={{marginBottom: '100px'}}>
         <Stack gap={3}>
-          <h1 className='text-center mt-5'>VU LFF</h1>
+          <h1 className='text-center'>VU LFF</h1>
           <h3 className='text-muted text-center'>Generate an LFF from your repository commit messages</h3>
           <GitHubUrlForm/>
           <FAQ/>
--- src/FAQ.tsx
+++ src/FAQ.tsx
@@ -6,14 +6,14 @@ const FAQ: React.FC = () => {
     <>
       <h3>Frequently Asked Questions</h3>
       <Accordion>
-      <Accordion.Item eventKey="0">
+        <Accordion.Item eventKey="0">
           <Accordion.Header>What's an LLF?</Accordion.Header>
           <Accordion.Body>
             An LFF is an acronym for the Lithuanian <i>Laiko Fiksavimo Forma</i> which translates to <i>Time Tracking Form</i>. This document keeps track of your progress while doing a programming task and is required for some module tasks at Vilnius University.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>How does this work?</Accordion.Header>
+          <Accordion.Header>How to use it?</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -25,7 +25,7 @@ const FAQ: React.FC = () => {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>I'm getting a rate limit error</Accordion.Header>
+          <Accordion.Header>How does this work?</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -37,6 +37,18 @@ const FAQ: React.FC = () => {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
+          <Accordion.Header>I'm getting an error</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="4">
           <Accordion.Header>Can I contribute?</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
